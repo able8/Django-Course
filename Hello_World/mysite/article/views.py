f63100d835d2a648cee1f302e6537fc62e408eac
able8 <able8@me.com>
2018-08-19 09:29:21 +0800

    p4 获取并显示文章内容和标题

--- Hello_World/mysite/article/views.py
+++ Hello_World/mysite/article/views.py
@@ -4,5 +4,5 @@ from .models import Article
 
 # Create your views here.
 def article_detail(request, article_id):
-    
-    return HttpResponse('文章 id：%s' % article_id)
+    article = Article.objects.get(id=article_id)
+    return HttpResponse('<h2>文章标题：%s </h2><hr> 文章内容：%s' % (article.title, article.content))
